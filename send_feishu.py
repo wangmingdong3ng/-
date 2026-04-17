434216751f648fc8bb33e37de01c641e23e0a46c
wangmingdong3ng <wangmingdong.ng@jiyunhudong.com>
2026-04-17 12:51:31 +0800

    Update send_feishu.py

--- send_feishu.py
+++ send_feishu.py
@@ -88,11 +88,11 @@ def create_card(message, is_weekend):
     
     # 根据工作日/周末选择不同的主题色和图标
     if is_weekend:
-        title = "🎉 周末祝福"
+        title = "🎉 到点了快跑呀！！！"
         color = "green"
         icon = "🌞"
     else:
-        title = "🌅 下班祝福"
+        title = "🌅 到点了快跑呀！！！"
         color = "blue"
         icon = "💼"
     
